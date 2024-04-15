92f547a9cf66e33bdda12394a371881c17ba26a5
Reidar Nilsen <4reidar@gmail.com>
2024-04-15 19:33:39 +0200

    Add project files.

---
+++ AIMVCTest/FaqPrediction.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Data;
+
+namespace AIMVCTest
+{
+    public class FaqPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public string Answer { get; set; }
+    }
+}
---
+++ AIMVCTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AIMVCTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
